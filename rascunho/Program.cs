863b8c5cb8400cfd7605fe07ba4cd4fb90ff008a
marcu <marcu@LAPTOP-MTO8J5A0>
2021-07-26 11:09:51 -0300

    Adicionar arquivos de projeto.

---
+++ rascunho/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BemVindo {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite quando jose tem: ");
+            double x = double.Parse(Console.ReadLine());
+            string resultado = (x == 100.00) ? "Jose tem 100 reais" : "Jose perdeu 100 reais";
+            Console.WriteLine(resultado);
+        }
+    }
+}
